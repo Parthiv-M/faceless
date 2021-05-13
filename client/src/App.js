2228e65b4a71f8e5806c7c0613529f759517e5ba
Pal Chheda <chhedapal@yahoo.in>
2021-05-13 15:15:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,16 @@ import JoinTeam from './Components/joinTeam';
 import Login from './Components/login';
 import Navbar from './Components/navbar';
 import Register from './Components/registration';
+import ScoreBoard from './Components/scoreBoard';
+import Play from './Components/play';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <About />
+      
       <CreateTeam />
-      <Login />
-      <Register />
-      <JoinTeam />
+      <ScoreBoard />
+      <Play />
     </div>
   );
 }
